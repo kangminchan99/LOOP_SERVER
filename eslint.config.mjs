cae332b3628461de73e31cc4e7a74d6b1c5fadb8
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2026-05-21 15:38:02 +0900

    refactor: eslint.config.mjs deprecated fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+const __dirname = new URL('.', import.meta.url).pathname;
+
+export default defineConfig(
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -20,7 +23,7 @@ export default tseslint.config(
       sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname,
       },
     },
   },
@@ -29,7 +32,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
